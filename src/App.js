5df410ff2ac261b1eecb03ca21bdd1a95c52035f
sinaloa10 <oscarsinaloa47@gmail.com>
2024-10-05 19:11:23 -0600

    cambios en la estructura solucion de errores

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import WelcomePage from './welcomePage'; // Importar tu componente
+import WelcomePage from './pages/welcomePage'; // Importar tu componente
 
 function App() {
   return (
     <Router>
       <Routes>
         {/* Ruta de la página de bienvenida */}
-        <Route path="/" element={<WelcomePage />} />
+        <Route path="./pages/welcomePage" element={<WelcomePage />} />
         {/* Otras páginas */}
+
       </Routes>
     </Router>
   );
